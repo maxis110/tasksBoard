b38206eb29133ab48a8114cc77852c8623fcfea6
maksim <mikhailenkomaxim@gmail.com>
2016-03-16 00:56:29 -0700

    Correct

--- taskBoard/app/urls.py
+++ taskBoard/app/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
-from app import views
+from serializer import UserSerializer, TaskSerializer, CategorySerializer
+from models import User, Task, Category
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = format_suffix_patterns([
-    url(r'^$', views.api_root),
-    url(r'^users/$', views.UserList.as_view(), name='user-list'),
-    url(r'^tasks/$', views.TaskList.as_view(), name='task-list'),
-    url(r'^category/$', views.CategoryList.as_view(), name='category-list'),
+    url(r'^users/$', ListCreateAPIView.as_view(queryset= User.objects.all(), serializer_class = UserSerializer), name='user-list'),
+    url(r'^tasks/$', ListCreateAPIView.as_view(queryset= Task.objects.all(), serializer_class = TaskSerializer), name='task-list'),
+    url(r'^category/$', ListCreateAPIView.as_view(queryset= Category.objects.all(), serializer_class = CategorySerializer), name='category-list'),
 ])
--- taskBoard/app/views.py
+++ taskBoard/app/views.py
@@ -6,24 +6,38 @@ from models import User, Category, Task
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework import renderers
 
 class UserList(generics.ListCreateAPIView):
-
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-class CategoryList(viewsets.ModelViewSet):
+    def get(self, request, *args, **kwargs):
+        users = self.get_object()
+        return Response(users)
 
+    def post(self, request, *args, **kwargs):
+       return self.create(request, *args, **kwargs)
+
+class CategoryList(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
-class TaskList(viewsets.ModelViewSet):
+    def get(self, request, *args, **kwargs):
+        category = self.get_object()
+        return Response(category)
 
+    def post(self, request, *args, **kwargs):
+       return self.create(request, *args, **kwargs)
+
+class TaskList(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
-@api_view(('GET',))
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format)
-    })
+    def get(self, request, *args, **kwargs):
+        tasks = self.get_object()
+        return Response(tasks)
+
+    def post(self, request, *args, **kwargs):
+       return self.create(request, *args, **kwargs)
+
